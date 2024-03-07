98ad257c7c916c3931f7aeef50edc02e6f1366a3
Danillo Soares <danillohss@gmail.com>
2024-03-07 01:39:40 -0300

    [FIX] - Resolvido o bug de não abrir o menu sanduiche no mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,42 @@
 <template>
-  <div>
-    <nav
-      class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg"
-      style="background-color: #2e948a"
-    >
-      <div class="container-fluid">
-        <i class="bi bi-camera-reels fs-2" style="padding-right: 10px"></i>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/">
-                Home
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link
-                class="nav-link active"
-                aria-current="page"
-                to="/favorites"
-                >Favorites</router-link
-              >
-            </li>
-          </ul>
-        </div>
+  <nav
+    class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg"
+    style="background-color: #2e948a"
+  >
+    <div class="container-fluid">
+      <i class="bi bi-camera-reels fs-2" style="padding-right: 10px"></i>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/">
+              Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/favorites"
+            >
+              Favorites
+            </router-link>
+          </li>
+        </ul>
       </div>
-    </nav>
-    <div style="padding-top: 70px"></div>
-  </div>
+    </div>
+  </nav>
+  <div style="padding-top: 70px"></div>
 </template>
 
 <script>
@@ -48,6 +47,6 @@ export default {
 
 <style>
 li:hover {
-  background-color: #5A8C87;
+  background-color: #606060;
 }
 </style>
